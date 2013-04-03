a439c01ff465a8a41f66ff9d1cbbbdbbe52aaf26
Bryan Schuetz <bschuetz@gmail.com>
2013-04-03 19:39:44 -0400

    partials and build optimization

--- source/javascripts/core.js
+++ source/javascripts/core.js
@@ -1,1 +1,13 @@
- 
+#= require 'respond.min.js'
+WebFontConfig = {
+	google: { families: [ 'Arvo:400,700:latin', 'Vollkorn:400italic,400,700:latin' ] }
+	};
+	(function() {
+	  var wf = document.createElement('script');
+	  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+	    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	  wf.type = 'text/javascript';
+	  wf.async = 'true';
+	  var s = document.getElementsByTagName('script')[0];
+	  s.parentNode.insertBefore(wf, s);
+})(); 
